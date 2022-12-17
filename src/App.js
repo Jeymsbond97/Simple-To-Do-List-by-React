9ac2e0e5366e40951a69104f68c58796ed3a6024
Tokhirbek Aminjonov <aminjonovtoxirbek@gmail.com>
2022-12-17 10:19:42 +0900

    Add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/navbar';
 
 const App = () => {
   return (
     <div>
       <h1>Hello Tokhirbek</h1>
+      <Navbar/>
     </div>
   );
 };
